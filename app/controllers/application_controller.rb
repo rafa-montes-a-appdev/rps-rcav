58e508468361f2977146bf3d1e150d2f247f6083
Rafael Montes <rmontes@chicagobooth.edu>
2023-04-09 16:15:59 +0000

    Template and links ready

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,5 +3,51 @@ class ApplicationController < ActionController::Base
 
   # Add your actions below this line
   # ================================
+  
+  def homepage
+    render({ :template => "game_templates/user_rules" })
+  end
+  
+  def play_rock
+
+    @comp_move = ["rock", "paper", "scissors"].sample
+      if @comp_move == "rock"
+        @outcome = "tied"
+      elsif @comp_move == "paper"
+        @outcome = "lost"
+      elsif @comp_move == "scissors"
+        @outcome = "won"
+      end 
+
+    render({ :template => "game_templates/user_rock" })
+  end
+
+  def play_paper
+        
+    @comp_move = ["rock", "paper", "scissors"].sample
+      if @comp_move == "rock"
+        @outcome = "won"
+      elsif @comp_move == "paper"
+        @outcome = "tied"
+      elsif @comp_move == "scissors"
+        @outcome = "lost"
+      end 
+
+    render({ :template => "game_templates/user_paper" })
+  end
+
+  def play_scissors
+
+    @comp_move = ["rock", "paper", "scissors"].sample
+    if @comp_move == "rock"
+      @outcome = "lost"
+    elsif @comp_move == "paper"
+      @outcome = "won"
+    elsif @comp_move == "scissors"
+      @outcome = "tied"
+    end 
+
+    render({ :template => "game_templates/user_scissors" })
+  end
 
 end
